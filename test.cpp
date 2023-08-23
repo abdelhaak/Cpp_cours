8360e7a015cf364cd8882938e46ed7a77c5cd317
abdelhaak <abdelhak.elalaoui53@gmail.com>
2023-08-23 13:59:29 +0200

    first commit

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    bool is_empty = false;
+
+    if (is_empty)       // if(is_empty == true)
+    cout << "Est Vide";
+
+    return 0;
+}
